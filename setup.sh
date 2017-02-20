535a933a8145bba2af959175573d12605b8472a2
sprayidle <sprayidle@gmail.com>
2017-02-20 22:54:33 +1300

    Add project specific setup script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 cp /source/setup.py /tmp/setup.py
 python /tmp/setup.py install
 
+# Run project setup
+/source/setup.sh
+
 # Add project to runit
 mkdir /etc/service/project
 cp /source/run.sh /etc/service/project/run
